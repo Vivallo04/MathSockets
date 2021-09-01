8c442ed282ac766332312538e90739012efcd890
Vivallo <andresvivallo4@gmail.com>
2021-08-31 19:29:26 -0600

    First Commit

---
+++ desktop/src/com/tec/mathsockets/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tec.mathsockets.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tec.mathsockets.MathSockets;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MathSockets(), config);
+	}
+}
