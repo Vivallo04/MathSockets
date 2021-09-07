0aa94cb24202cbf674021539ec3abb27295cee3a
Vivallo <andresvivallo4@gmail.com>
2021-09-07 13:18:05 -0600

    Add DoublyLinkedList and NTile node

---
+++ core/src/com/tec/mathsockets/states/game/tiles/Tile.java
@@ -0,0 +1,17 @@
+package com.tec.mathsockets.states.game.tiles;
+
+public class Tile {
+
+    enum TileType {
+        START_TILE,
+        CHALLENGE_TILE,
+        TRAP_TILE,
+        TUNNEL_TILE,
+        WIN_TILE
+    }
+
+    public Tile() {
+
+    }
+
+}
