0ceb98c319d74cb7515509df7b773ae1b2c0c05f
Noemí Vargas <mimi2002@estudiantec.cr>
2021-09-20 21:15:56 -0600

    Creación de los challenges

--- core/src/com/tec/mathsockets/states/challenge/Challenge.java
+++ core/src/com/tec/mathsockets/states/challenge/Challenge.java
@@ -1,14 +1,154 @@
 package com.tec.mathsockets.states.challenge;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Challenge {
 
-    private final Random random = new Random();
-
-    public Challenge() {
-
+    public enum Operation {
+        SUMA,
+        RESTA,
+        MULTIPLICACION,
+        DIVISION
     }
 
+    Scanner resultadoJugador = new Scanner(System.in);
+    static String problem;
+
+    int userInput;
+    private Operation challengeType;
+    private int num0;
+    private int num1;
+    private int num2;
+    //private int operacion = (int) (Math.random() * 4 + 1);
+    private int result;
+
+    public Challenge() {
+        this.num1 = (int) (Math.random() * 50 + 1);
+        this.num2 = (int) (Math.random() * 50 + 1);
+        askForChallenge();
+    }
+
+
+    /**
+     *
+     * @param num1
+     */
+    public void setNum1(int num1) {
+        if (num1 < num2) {
+            this.num0 = num1;
+            this.num1 = num2;
+            this.num2 = num0;
+            return;
+        }
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    /*
+    public void getOperador() {
+        if (operacion == 1) {
+            problem = "¡Suma los dos números!";
+        } else if (operacion == 2) {
+            problem = "Resta los dos números";
+        } else if (operacion == 3) {
+            problem = "Multiplica los dos números";
+        } else {
+            problem = "Divide los dos números";
+        }
+        return;
+    }*/
+
+
+    /**
+     * Get num1 value to string
+     * @return
+     */
+    public String getNum1(){
+        return String.valueOf(num1);
+    }
+
+
+    /**
+     * Get num2 value to string
+     * @return num2 st
+     */
+    public String getNum2() {
+        return String.valueOf(num2);
+    }
+
+    /**
+     * Get a random operacion from Operation Enum
+     * @return Random operation type
+     */
+    private Operation getRandomOperation() {
+        return Operation.values()[new Random().nextInt(Operation.values().length)];
+    }
+
+
+
+    /**
+     *
+     * @return
+     */
+    public void askForChallenge() {
+        if (getRandomOperation().equals(Operation.SUMA)) {
+            System.out.println("La operación es suma de: " + num1 + " + " + num2);
+            result = num1 + num2;
+            System.out.println(TAG + " Suma resultado: " + result);
+            userInput = resultadoJugador.nextInt();
+
+            if (userInput == result) {
+                System.out.println("Correcto!");
+                return;
+            }
+            System.out.println("Oops");
+
+        } else if (getRandomOperation().equals(Operation.RESTA)) {
+            System.out.println("La operación es resta de: " + num1 + " - " + num2);
+            result = num1 - num2;
+            System.out.println(TAG + " resta resultado: " + result);
+            userInput = resultadoJugador.nextInt();
+            if (userInput == result) {
+                System.out.println("Correcto!");
+                return;
+            }
+            System.out.println("Oops");
+
+        } else if (getRandomOperation().equals(Operation.MULTIPLICACION)) {
+            System.out.println("La operación es multiplicación de: " + num1 + " * " + num2);
+            result = num1 * num2;
+            System.out.println(TAG + " multiplicación resultado: " + result);
+            userInput = resultadoJugador.nextInt();
+            if (userInput == result) {
+                System.out.println("Correcto!");
+                return;
+            }
+            System.out.println("Oops");
+
+        } else {
+            System.out.println("La operación es división de: " + num1 + " / " + num2);
+            setNum1(num1);
+            result = num1 / num2;
+            System.out.println(TAG + " división resultado: " + result);
+            userInput = resultadoJugador.nextInt();
+            if (userInput == result) {
+                System.out.println("Correcto!");
+                return;
+            }
+            System.out.println("Oops");
+
+        }
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Challenge challenge = new Challenge();
+
+    }
 
 }
--- core/src/com/tec/mathsockets/states/challenge/ChallengeState.java
+++ core/src/com/tec/mathsockets/states/challenge/ChallengeState.java
@@ -1,11 +1,25 @@
 package com.tec.mathsockets.states.challenge;
 
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.tec.mathsockets.MathSockets;
 import com.tec.mathsockets.states.State;
+
+import java.awt.*;
+
+import static com.tec.mathsockets.states.challenge.Challenge.problem;
 
 public class ChallengeState extends State {
 
     private final MathSockets game;
+    //Challenge numeros = new Challenge();
+    ShapeRenderer rectangle;
+    public SpriteBatch batch;
+    private BitmapFont font;
+    private TextField text;
+    int x = 200;
+    int y = 200;
 
     public ChallengeState(MathSockets game) {
         this.game = game;
@@ -14,18 +28,45 @@ public class ChallengeState extends State {
 
     public void create() {
 
+        rectangle = new ShapeRenderer();
+        font = new BitmapFont();
+        //TextButton verifButton = new TextButton("Verficar resultado");
+        //verification.setPosition(x, x);
+        //verification.setSize(x, x);
+
+        text = new TextField("");
+        //text.setPosition(x, x);
+        //text.setSize(x, x);
     }
 
 
     @Override
-    public void render(float delta) {
-        super.render(delta);
+    public void render(float delta) { //Aquí se crea la nueva ventana y se le agregan los datos
+        super.render(delta); //Heredo el método render de State
+
+        //Crear el rectángulo con la pregunta y el campo para ingresar los datos
+        //después verifica si el resultado es el correcto y procede con el juego
+        rectangle.begin(ShapeRenderer.ShapeType.Filled);
+        rectangle.rect(x, y,1200, 602 );
+        batch.begin();
+            font.draw(batch, problem, 550, 500);
+            //font.draw(batch, "Número 1: "+ numeros.getNum1(), 500, 530);
+            //font.draw(batch, "Número 2: "+ numeros.getNum2(), 550, 530);
+        batch.end();
+        //Agregar aquí el TextField y comprobar con el métdodo Challenge
+        //add verifButton
+        //add text, obtener el dato (creo que pasarlo a int) y comprarlo con el método Challenge de la clase Challenge
+
+
+        rectangle.end();
+
+
 
 
     }
 
     @Override
-    public void dispose() {
+    public void dispose() { // Una manera de manejar memoria
         super.dispose();
     }
 
