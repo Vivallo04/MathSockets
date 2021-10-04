49e591ca16e535651ac6a9cee37a2459a4eb78b2
Vivallo <andresvivallo4@gmail.com>
2021-10-04 00:00:17 -0600

    [Add] ChooseAvatarState constructor

---
+++ core/src/com/tec/mathsockets/states/ChooseAvatarState.java
@@ -0,0 +1,12 @@
+package com.tec.mathsockets.states;
+
+import com.tec.mathsockets.MathSockets;
+
+public class ChooseAvatarState extends State {
+
+    private final MathSockets game;
+
+    public ChooseAvatarState(MathSockets game) {
+        this.game = game;
+    }
+}
